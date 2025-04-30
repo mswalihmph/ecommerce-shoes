d8c2b252021a10951fb225547465eda2ed89e499
mswalihmph <swalihmph@gmail.com>
2025-04-30 09:17:53 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const state={
 
 
 module.exports.connect=function(done){
-    const url='mongodb://localhost:27017';
+    const url=;
     const dbname='shopping'
     MongoClient.connect(url).then(
         (client)=>{
